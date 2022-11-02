7162e46ee322cef07d82550201090c9bda4105f3
hamzaali00 <100699289+hamzaali00@users.noreply.github.com>
2022-11-02 15:35:26 -0400

    cleaned schema

--- schema.sql
+++ schema.sql
@@ -13,12 +13,11 @@ CREATE TABLE death_by_state (
 
 -----------------------
 
+-- create id column and set as primary key, no unique values in our dataset
 ALTER TABLE covid_data ADD COLUMN id SERIAL PRIMARY KEY;
 ALTER TABLE death_by_state ADD COLUMN id SERIAL PRIMARY KEY;
 
-SELECT * FROM covid_data;
-SELECT * FROM death_by_state;
-
+-- join datasets covid_data and death_by_state on id
 SELECT dbs.state,
 	cd.age_group,
 	cd.condition_group,
@@ -29,5 +28,3 @@ INTO covid_state
 FROM death_by_state as dbs
 JOIN covid_data as cd
 ON dbs.id = cd.id;
-
-SELECT * FROM covid_state;
